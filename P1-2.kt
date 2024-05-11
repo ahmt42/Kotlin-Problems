1c024c0fc44dd85450f4ae555cae05c66444289f
Ahmet Güleç <49699262+ahmt42@users.noreply.github.com>
2024-05-11 22:00:48 +0300

    Delete P1-2.kt

--- P1-2.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val metin = "Merhaba Dünya!"
-    val tersMetin = tersCevir(metin)
-    println(tersMetin) // !alnDü ahrebaM
-}
-
-fun tersCevir(metin: String): String {
-	return metin.reversed()
-}
