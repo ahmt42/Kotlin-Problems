d8bc2c57babe333b2e17a7b6b73639439c7f9c7b
Ahmet Güleç <49699262+ahmt42@users.noreply.github.com>
2024-05-16 10:00:33 +0300

    Create P2-2.kt
    Solution 2
    Bir metin dosyasındaki kelimelerin sıklıklarını saydırın ve en sık kullanılan 10 kelimeyi yazdırın.

---
+++ src/P2-2.kt
@@ -0,0 +1,21 @@
+// 2. çözüm (TreeMap kullanarak)
+
+import java.io.File
+
+fun main() {
+    val dosya = File("metin.txt")
+    val kelimeSayac = TreeMap<String, Int>()
+
+    dosya.forEachLine { satir ->
+        val kelimeler = satir.split(" ") // kelimeleri ayır
+        for (kelime in kelimeler) {
+            val kucukKelime = kelime.lowercase() // kelimeyi küçük harfe dönüştür
+            kelimeSayac[kucukKelime] = kelimeSayac.getOrDefault(kucukKelime, 0) + 1
+        }
+    }
+
+    // En sık kullanılan 10 kelimeyi yazdır
+    for ((kelime, sayac) in kelimeSayac.descendingMap().take(10)) {
+        println("$kelime: $sayac")
+    }
+}
